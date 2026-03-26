7aa6b1f37651e0f352e424801c4151a56527c60c
Shashank Mulukalla <115165822+Shashi0013@users.noreply.github.com>
2026-03-26 23:51:58 +0530

    Add script to start Docker container for Python app

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+# Pull the Docker image from Docker Hub
+docker pull shashi2002/python-project-app
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 shashi2002/python-project-app
